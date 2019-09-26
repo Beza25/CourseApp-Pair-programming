078394333260efb88c6c4a666cd331842f006725
danielgkneto <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-26 10:03:25 -0400

    Initial commit

---
+++ src/main/java/com/mcjavabc/coursestudentapp/CourseStudentAppApplication.java
@@ -0,0 +1,13 @@
+package com.mcjavabc.coursestudentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseStudentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseStudentAppApplication.class, args);
+    }
+
+}
